8cb32f01e24204fb5fc3da5e835d8d9a01b3ddac
Gsllchb <Gsllchb@gmail.com>
2018-07-20 13:42:15 +0800

    Update and rename GBus-count.java to GBusCount.java

--- src/GBusCount.java
+++ src/GBusCount.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class GBusCount {
   public static void main(String[] args) {
     Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
     int t = in.nextInt();
